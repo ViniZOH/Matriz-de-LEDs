25753f770f2fedb6ba781d9a8027900eecb4e85e
João Murillo <joaomurilloxd@gmail.com>
2025-01-26 18:17:22 -0300

    Adição de frames
    Adição de mais 2 frames a animação 4

--- matriz_de_leds/bibliotecas/anim4.h
+++ matriz_de_leds/bibliotecas/anim4.h
@@ -21,24 +21,35 @@ double anim4_blue[25] =  {0.0, 0.5, 0.5, 0.5, 0.0,
                           0.0, 0.0, 1.0, 1.0, 0.0,
                           0.0, 1.0, 1.0, 0.0, 0.0};
 
-// Frames da animação da nuvem emitindo raio
 uint anim4_f1[25] = {0, 1, 1, 1, 0,
                      1, 1, 1, 1, 1,
                      0, 0, 0, 2, 2,
                      0, 0, 2, 2, 0,
-                     0, 2, 2, 0, 0};
+                     0, 2, 2, 0, 0};// Frames da animação da nuvem emitindo raio direita
 
 uint anim4_f2[25] = {0, 1, 1, 1, 0,
                      1, 1, 1, 1, 1,
-                     2, 2, 0, 0, 0,
-                     0, 2, 2, 0, 0,
-                     0, 0, 2, 2, 0};
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0}; // Somente a nuvem acesa
 
 uint anim4_f3[25] = {0, 1, 1, 1, 0,
                      1, 1, 1, 1, 1,
+                     2, 2, 0, 0, 0,
+                     0, 2, 2, 0, 0,
+                     0, 0, 2, 2, 0};// Frames da animação da nuvem emitindo raio esquerda
+
+uint anim4_f4[25] = {0, 1, 1, 1, 0,
+                     1, 1, 1, 1, 1,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0,
+                     0, 0, 0, 0, 0}; // Somente a nuvem acesa
+
+uint anim4_f5[25] = {0, 1, 1, 1, 0,
+                     1, 1, 1, 1, 1,
                      0, 2, 0, 2, 0,
                      0, 0, 2, 0, 0,
-                     0, 2, 2, 2, 0};
+                     0, 2, 2, 2, 0};// Frames da animação da nuvem emitindo raio meio
 
 // Função para rodar a animação
 void play_anim4(PIO pio, uint sm, uint32_t valor_led) {
@@ -47,6 +58,10 @@ void play_anim4(PIO pio, uint sm, uint32_t valor_led) {
     put_leds(anim4_f2, anim4_red, anim4_green, anim4_blue, valor_led, pio, sm);
     sleep_ms(200);
     put_leds(anim4_f3, anim4_red, anim4_green, anim4_blue, valor_led, pio, sm);
+    sleep_ms(200);
+    put_leds(anim4_f4, anim4_red, anim4_green, anim4_blue, valor_led, pio, sm);
+    sleep_ms(200);
+    put_leds(anim4_f5, anim4_red, anim4_green, anim4_blue, valor_led, pio, sm);
     sleep_ms(200);
 }
 
